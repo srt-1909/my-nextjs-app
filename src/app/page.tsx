d31adccaf5140e7645bfbb30fcdb3ea1c778a5b1
RajviKamani <rajvikamani2211@gmail.com>
2025-01-21 15:33:47 +0530

    name changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
 
-    My Name is : Rajvi <br></br>
+    My Name is : Rajvi Kamani <br></br>
     My Name is : Arati <br></br>
     My Name is :  <br></br>
     <br></br>

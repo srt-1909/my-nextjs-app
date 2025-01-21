4512ed70a561cba0bbd33a1a312414a1df412099
mudra <mudrab@zenithive.com>
2025-01-21 15:21:18 +0530

    name changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-
-
 export default function Home() {
   return (
     <>
-
-    My Name is : Rajvi <br></br>
-    My Name is :  <br></br>
-    My Name is : Mudra <br></br>
-    <br></br>
-     
-    
-    
+      My Name is : Rajvi <br></br>
+      My Name is : <br></br>
+      My Name is : Mudra <br></br>
+      <br></br>
     </>
   );
 }
